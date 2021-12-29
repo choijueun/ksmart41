fb07df570cdd60c14647f2d2b66215bd227e8183
CSH <seonhui420@gmail.com>
2021-12-29 17:00:03 +0900

    [최선희] 전자세금계산서 및 회원,로그인 controller, service파일 생성/sidebar 화면 연결
    file: InvoiceController.java/LoginController.java/
    UserRegController.java
    detail: 전자세금계산서 및 회원,로그인 controller, service파일 생성/sidebar 화면 연결

---
+++ ks41gi1team/src/main/java/k1/smart/team/controller/csh/LoginController.java
@@ -0,0 +1,29 @@
+package k1.smart.team.controller.csh;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import k1.smart.team.service.csh.LoginService;
+
+@Controller
+@RequestMapping(value="/k1Login")
+public class LoginController {
+	private LoginService loginService;
+	
+	public LoginController(LoginService loginService) {
+		this.loginService = loginService;
+	}
+	
+	//로그인 내역
+	@GetMapping("")
+	public String getLoginList(Model model) {
+		
+		return "login/login_list";
+	}
+	
+
+}
