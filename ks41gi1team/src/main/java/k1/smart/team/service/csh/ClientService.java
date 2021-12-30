ef98e5ef09ed8b3b89c13f112bc6f02c1e0a915b
CSH <seonhui420@gmail.com>
2021-12-30 10:01:51 +0900

    [최선희] 거래처controller,service 파일 수정
    file:CilentController.java/ClientService.java
    detail: 거래처controller,service 파일 수정

--- ks41gi1team/src/main/java/k1/smart/team/service/csh/ClientService.java
+++ ks41gi1team/src/main/java/k1/smart/team/service/csh/ClientService.java
@@ -19,5 +19,17 @@ public class ClientService {
 		this.clientMapper = clientMapper;
 	}
 	
+	//거래처 전체 목록 조회
+	public List<Client> getAllClientList(String clientCode) {
+		clientList = clientMapper.getAllClientList(clientCode);
+		String clientNum;
+		for(int i=0; i<clientList.size(); i++) {
+			
+		}
+		
+		
+		return clientList;
+	}
+	
 
 }
