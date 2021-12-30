46bd142cef7ecc80bddf4c66182608210eaf4c77
CJE <songjueun815@gmail.com>
2021-12-30 09:59:47 +0900

    [최주은] codemapper 확장 삭제...

--- ks41gi1team/src/main/java/k1/smart/team/mapper/cje/StockMapper.java
+++ ks41gi1team/src/main/java/k1/smart/team/mapper/cje/StockMapper.java
@@ -8,13 +8,10 @@ import k1.smart.team.dto.cje.Stock;
 import k1.smart.team.mapper.CodeMapper;
 
 @Mapper
-public interface StockMapper extends CodeMapper {
+public interface StockMapper {
 	//재고정보 전체조회
 	public List<Stock> getAllStockList(String mainBusinessCode);
 	
 	//재고정보 상세조회
 	public List<Stock> getStockInfo(String mainBusinessCode, String inventoryCode);
-	
-	//기본키 숫자부분 새로 형성(마지막 값 +1)
-	public String getNewCodeNum(String codeName, String tableName);
 }
