a2abcbe1d17a7716dde74bff3f539fd0503af436
ECS <ECS@DESKTOP-048HU3O>
2021-12-30 09:56:01 +0900

    [박재형] 코스트 작업(리스트)

--- ks41gi1team/src/main/java/k1/smart/team/service/pjh/CostService.java
+++ ks41gi1team/src/main/java/k1/smart/team/service/pjh/CostService.java
@@ -2,9 +2,12 @@ package k1.smart.team.service.pjh;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import k1.smart.team.dto.pjh.Cost;
 import k1.smart.team.mapper.pjh.CostMapper;
 
+@Service
 public class CostService {
 	private CostMapper costMapper;
 	private List<Cost> costList;
@@ -16,12 +19,13 @@ public class CostService {
 	
 	public List<Cost> getAllCostList(String mainBusinessCode) {
 		costList = costMapper.getAllCostList(mainBusinessCode);
+		System.out.println(costList);
 		
-		String costNum;
-		for(int i=0; i<costList.size(); i++) {
-			costNum = costList.get(i).getEtcPurchaseCode().replace("etcPurchaseCode_", "");
-			costList.get(i).setEtcPurchaseCode(costNum);
-		}
+		/*
+		 * String costNum; for(int i=0; i<costList.size(); i++) { costNum =
+		 * costList.get(i).getEtcPurchaseCode().replace("etcPurchaseCode_", "");
+		 * costList.get(i).setEtcPurchaseCode(costNum); }
+		 */
 		
 		return costList;
 	}
