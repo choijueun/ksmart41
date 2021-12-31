db5fe397b947c3402111ebe3b36fcb94b366ff0f
ECS <ECS@DESKTOP-048HU3O>
2021-12-31 17:35:53 +0900

    [박재형] 조회쪽 화면연결 끝

--- ks41gi1team/src/main/java/k1/smart/team/controller/pjh/TotalAcController.java
+++ ks41gi1team/src/main/java/k1/smart/team/controller/pjh/TotalAcController.java
@@ -1,24 +1,34 @@
 package k1.smart.team.controller.pjh;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import k1.smart.team.dto.pjh.Cost;
+import k1.smart.team.dto.pjh.TotalAc;
 import k1.smart.team.service.pjh.TotalAcService;
 
 @Controller
 @RequestMapping(value = "/k1TotalAc")
 public class TotalAcController {
 	private TotalAcService totalAcService;
+	private String mainBusinessCode;
+	
 	public TotalAcController(TotalAcService totalAcService) {
 		this.totalAcService = totalAcService;
 	}
 	
 	@GetMapping("")
 	public String totalAcMain(Model model) {
+		mainBusinessCode = "fac_ksmartSeoul_Seoul_001";
+		List<TotalAc> totalAcList = totalAcService.getTotalAcList(mainBusinessCode);
+		
 		model.addAttribute("title","통합회계관리");
+		model.addAttribute("totalAcList",totalAcList);
 		return "totalAc/totalAc_list";
 	}
 	
