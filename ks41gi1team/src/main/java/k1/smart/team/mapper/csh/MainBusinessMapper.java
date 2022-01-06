d1242ba25f8dfd152ab86a1132da28b5599b6ad6
CSH <seonhui420@gmail.com>
2022-01-06 09:42:14 +0900

    [최선희] 사업장 목록 작업 file: MainBusiness관련 파일 전부 detail: MainBusiness관련 파일 전체 작업

---
+++ ks41gi1team/src/main/java/k1/smart/team/mapper/csh/MainBusinessMapper.java
@@ -0,0 +1,11 @@
+package k1.smart.team.mapper.csh;
+
+import java.util.List;
+
+import k1.smart.team.dto.csh.MainBusiness;
+
+public interface MainBusinessMapper {
+
+	List<MainBusiness> getAllMainBusinessList(String mainBusinessCode);
+
+}
