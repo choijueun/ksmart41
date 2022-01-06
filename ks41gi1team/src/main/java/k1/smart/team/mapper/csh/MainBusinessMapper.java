247cf4e038cc23c91347760839d6317cb099f670
CSH <seonhui420@gmail.com>
2022-01-06 10:19:59 +0900

    [최선희] MainBusinessMapper 에러 수정 file: MainBusinessMapper.java detail: MainBusinessMapper 겟매핑

--- ks41gi1team/src/main/java/k1/smart/team/mapper/csh/MainBusinessMapper.java
+++ ks41gi1team/src/main/java/k1/smart/team/mapper/csh/MainBusinessMapper.java
@@ -2,8 +2,10 @@ package k1.smart.team.mapper.csh;
 
 import java.util.List;
 
-import k1.smart.team.dto.csh.MainBusiness;
+import org.apache.ibatis.annotations.Mapper;
 
+import k1.smart.team.dto.csh.MainBusiness;
+@Mapper
 public interface MainBusinessMapper {
 
 	List<MainBusiness> getAllMainBusinessList(String mainBusinessCode);
