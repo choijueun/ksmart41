0ad25860c03b6e3b44a619fb751a56d1ec8d3cbe
CJE <songjueun815@gmail.com>
2022-01-12 15:51:45 +0900

    [최주은] CodeMapper

--- ks41gi1team/src/main/java/k1/smart/team/common/CommonUtils.java
+++ ks41gi1team/src/main/java/k1/smart/team/common/CommonUtils.java
@@ -1,39 +1,26 @@
 package k1.smart.team.common;
 
 import java.util.Map;
-
 import java.util.List;
-
-
 
 public class CommonUtils {		
 
-	/*
-
-	 * 공백 또는 null 체크
-
+	/**
+	 * 공백·NULL: TRUE
+	 * 유효: FALSE
 	 */
-
 	public static boolean isEmpty(Object obj) {
-
 		if(obj == null) return true;
-
-		if ((obj instanceof String) && (((String)obj).trim().length() == 0)) { return true; }
-
-	        if (obj instanceof Map) { return ((Map<?, ?>) obj).isEmpty(); }
-
-	        if (obj instanceof Map) { return ((Map<?, ?>)obj).isEmpty(); } 
-
-	        if (obj instanceof List) { return ((List<?>)obj).isEmpty(); }
-
-	        if (obj instanceof Object[]) { return (((Object[])obj).length == 0); }
-
-
+		if ((obj instanceof String) && (((String)obj).trim().length() == 0)) {
+										return true;
+		}
+        if (obj instanceof Map) 		return ((Map<?, ?>)obj).isEmpty();
+        if (obj instanceof List) 		return ((List<?>)obj).isEmpty(); 
+        if (obj instanceof Object[]) 	return (((Object[])obj).length == 0); 
 
 		return false;
-
 	}
-
+	
 }
 
 
