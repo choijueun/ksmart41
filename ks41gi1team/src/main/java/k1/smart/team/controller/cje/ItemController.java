dc0fc6c5789c5c5c6d11061fb99688c55708109b
CJE <songjueun815@gmail.com>
2022-01-18 16:53:16 +0900

    [최주은] 삭제버튼

--- ks41gi1team/src/main/java/k1/smart/team/controller/cje/ItemController.java
+++ ks41gi1team/src/main/java/k1/smart/team/controller/cje/ItemController.java
@@ -206,6 +206,10 @@ public class ItemController {
 		return "redirect:/k1Item";
 	}
 	
+	/**
+	 * 카테고리 목록 조회
+	 * @param model
+	 */
 	@GetMapping("/k1ItemCategory")
 	public String itemCategory(Model model) {
 		itemList = itemService.getAllCategories(mainBusinessCode);
@@ -216,5 +220,12 @@ public class ItemController {
 		model.addAttribute("SectionLocation", "카테고리");
 		
 		return "stock/item/item_category";
+	}
+	
+	@PostMapping("/k1ItemRemove")
+	public String removeItem(String itemCode) {
+		//삭제진행
+		
+		return "redirect:/k1Item";
 	}
 }
