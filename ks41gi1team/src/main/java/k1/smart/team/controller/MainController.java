7c650ce8eee4960f7f488001dcde6ee738331738
CJE <songjueun815@gmail.com>
2022-01-18 13:53:27 +0900

    [최주은] 소개페이지 및 메인화면 수정

--- ks41gi1team/src/main/java/k1/smart/team/controller/MainController.java
+++ ks41gi1team/src/main/java/k1/smart/team/controller/MainController.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class MainController {
 
 	@GetMapping("/")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/main")
+	public String main() {
+		return "main";
 	}
 }
