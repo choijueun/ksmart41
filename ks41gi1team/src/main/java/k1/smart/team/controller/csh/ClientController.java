9ca5f65a2929df6fc7dc26a3e7e0b50464e524e0
CSH <seonhui420@gmail.com>
2022-01-24 12:18:34 +0900

    [최선희] 로그인화면, 회원가입 화면
    file: LoginController.java login.html signUp.html
    detail: 로그인화면(login.html), 회원가입 화면(signUp.html)

--- ks41gi1team/src/main/java/k1/smart/team/controller/csh/ClientController.java
+++ ks41gi1team/src/main/java/k1/smart/team/controller/csh/ClientController.java
@@ -35,64 +35,12 @@ public class ClientController {
 		return "clientBusiness/client_list";
 	};
 	
-	//거래처 전체목록에서 검색
-	@PostMapping("/clientList")
-	public String getSearchClientList(@RequestParam(value = "searchKey", required = false) String searchKey
-									,@RequestParam(value = "searchValue", required = false) String searchValue
-									,Model model) {
-		if(searchKey != null && "clientCode".equals(searchKey)) {
-			searchKey = "clientCode";
-		}else if(searchKey != null && "mainBusinessCode".equals(searchKey)) {
-			searchKey = "mainBusinessCode";
-		}else if(searchKey != null && "businessCategory".equals(searchKey)) {
-			searchKey = "businessCategory";
-		}else if(searchKey != null && "userId".equals(searchKey)) {
-			searchKey = "userId";
-		}else if(searchKey != null && "businessName".equals(searchKey)) {
-			searchKey = "businessName";
-		}else if(searchKey != null && "businessRepresentative".equals(searchKey)) {
-			searchKey = "businessRepresentative";
-		}else if(searchKey != null && "businessType1".equals(searchKey)) {
-			searchKey = "businessType1";
-		}else if(searchKey != null && "businessType2".equals(searchKey)) {
-			searchKey = "businessType2";
-		}else if(searchKey != null && "businessNum".equals(searchKey)) {
-			searchKey = "businessNum";
-		}else if(searchKey != null && "businessCount".equals(searchKey)) {
-			searchKey = "businessCount";
-		}else if(searchKey != null && "businessMobile".equals(searchKey)) {
-			searchKey = "businessMobile";
-		}else if(searchKey != null && "businessPlace".equals(searchKey)) {
-			searchKey = "businessPlace";
-		}else if(searchKey != null && "businessEmail".equals(searchKey)) {
-			searchKey = "businessEmail";
-		}else if(searchKey != null && "regDate".equals(searchKey)) {
-			searchKey = "regDate";
-		}else {
-			searchKey = "updateDate";
-		};
-		//검색키, 검색어를 통해 거래처목록 조회
-		System.out.println(searchKey);
-		System.out.println(searchValue);
-		List<Client> clientList = clientService.getClientListBySearchKey(searchKey, searchValue);
-		System.out.println(clientList);
-		//조회된 회원목록을 model에 값 저장
-		model.addAttribute("SectionTitle", "거래처목록");
-		model.addAttribute("clientList", clientList);
-		return "clientBusiness/client_list";
-		
-	}
-	
-	
 	//거래처 등록 아이디 조회
 	@GetMapping("/clientRegister")
 	public String clientRegister(Model model) {
 		model.addAttribute("SectionTitle", "거래처 등록");
 		return "clientBusiness/client_register";
 	}
-	
-	//거래처 등록 모달
-	
 	
 	//거래처 상세정보
 	@GetMapping("/clientDetail/{clientCode}")
