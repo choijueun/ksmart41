f16a6032f695b8772e9aaa66b051dbd55facafa1
PSB <PSB@git-hub>
2022-01-28 17:05:46 +0900

    [박수빈] 계약서 수정화면 작업중, 수정버튼누르고 수정화면 갈시 500에러

--- ks41gi1team/src/main/java/k1/smart/team/mapper/psb/MaterialOrderMapper.java
+++ ks41gi1team/src/main/java/k1/smart/team/mapper/psb/MaterialOrderMapper.java
@@ -10,13 +10,9 @@ import k1.smart.team.dto.psb.MaterialOrderInfo;
 @Mapper
 public interface MaterialOrderMapper {
 
-	//발주 정보 수정
-	public int modifyMaterialOrderInfo(MaterialOrder materialOrder);
+
 	
-	/*
-	 * //발주내역 상세조회(하나일경우) public MaterialOrderInfo getMaterialOrderInfo(String
-	 * mainBusinessCode, String materialOrderCode);
-	 */
+
 	
 	//발주내역 상세조회(여러개 일경우)
 	public List<MaterialOrderInfo> getMaterialOrderInfoList(String mainBusinessCode, String materialOrderCode);
@@ -24,15 +20,11 @@ public interface MaterialOrderMapper {
 	//발주 상세
 	public MaterialOrder getMaterialOrderInfo(String materialOrderCode);
 	
-	
-
-
-	
 	//발주 전체 조회
 	public List<MaterialOrder> getMaterialOrderList(String mainBusinessCode);
 
 	//발주 수정 조회
-	public int modifyMaterilOrderInfo(MaterialOrder materialOrder);
+	public int modifyMaterialOrder(MaterialOrder materialOrder);
 	
 	//발주 정보 조회
 	public MaterialOrder getMaterialOrderInfoByMaterialOrderCode(String materialOrderCode);
