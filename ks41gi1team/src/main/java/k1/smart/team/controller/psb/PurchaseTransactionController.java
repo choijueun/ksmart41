39a7e26ed815f1fc666c4b379e84489a378887d5
PSB <PSB@git-hub>
2022-01-28 17:52:34 +0900

    [박수빈] 비용거래명세서 수정화면 컨트롤러 작업중

--- ks41gi1team/src/main/java/k1/smart/team/controller/psb/PurchaseTransactionController.java
+++ ks41gi1team/src/main/java/k1/smart/team/controller/psb/PurchaseTransactionController.java
@@ -3,16 +3,32 @@ package k1.smart.team.controller.psb;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import k1.smart.team.dto.cje.Stock;
+import k1.smart.team.dto.csh.Client;
+import k1.smart.team.dto.csh.MainBusiness;
+import k1.smart.team.dto.csh.User;
+import k1.smart.team.dto.psb.Contract;
+import k1.smart.team.dto.psb.MaterialOrder;
 import k1.smart.team.dto.psb.PurchaseTransaction;
 import k1.smart.team.dto.psb.SalesTransaction;
+import k1.smart.team.service.cje.ItemService;
+import k1.smart.team.service.csh.ClientService;
+import k1.smart.team.service.csh.MainBusinessService;
+import k1.smart.team.service.csh.UserService;
+import k1.smart.team.service.psb.ContractService;
+import k1.smart.team.service.psb.MaterialOrderService;
+import k1.smart.team.service.psb.ProductOrderService;
 import k1.smart.team.service.psb.PurchaseTransactionService;
 import k1.smart.team.service.psb.SalesTransactionService;
 
@@ -20,16 +36,91 @@ import k1.smart.team.service.psb.SalesTransactionService;
 @RequestMapping(value="/k1PurchaseTransaction")
 public class PurchaseTransactionController {
 
+	private static final Logger log = LoggerFactory.getLogger(MaterialOrderController.class);
+	
+	
 	private PurchaseTransactionService purchaseTransactionService;
+	private ClientService clientService;
+	private MainBusinessService mainBusinessService;
+	private UserService userService;
+	private ItemService itemService;
+	private ContractService contractService;
 	private SalesTransactionService salesTransactionService;
 	private String mainBusinessCode;
 	private PurchaseTransaction purchaseTransactionInfo;
 	private List<PurchaseTransaction> purchaseTransactionInfoList;
 	private Map<String, Object> resultMap;
+
+
+	private Map<String, Object> paramMap;
 	
-	public PurchaseTransactionController(PurchaseTransactionService purchaseTransactionService, SalesTransactionService salesTransactionService) {
+	public PurchaseTransactionController(PurchaseTransactionService purchaseTransactionService, SalesTransactionService salesTransactionService,
+			ClientService clientService, ItemService itemService, MainBusinessService mainBusinessService, UserService userService, ContractService contractService) {
 		this.purchaseTransactionService = purchaseTransactionService;
 		this.salesTransactionService = salesTransactionService;
+		this.clientService = clientService;
+		this.contractService = contractService;
+		this.itemService = itemService;
+		this.mainBusinessService = mainBusinessService;
+		this.userService = userService;
+	}
+	
+	//비용 수정 
+	@GetMapping("/modify/{purchaseTsCode}")
+	public String modifyPurchaseTransaction(
+			@RequestParam(value="purchaseTsCode", required=false) String purchaseTsCode
+			,Model model) {
+		
+		
+		log.info("modifyPurchaseTransaction purchaseTsCode: {}", purchaseTsCode);
+
+		
+	
+		 PurchaseTransaction purchaseTransactionInfo = purchaseTransactionService.getPurchaseTransactionInfoByCode(purchaseTsCode);
+			model.addAttribute("purchaseTransactionInfo", purchaseTransactionInfo);
+			System.out.println("purchaseTransactionInfo-->" + purchaseTransactionInfo);
+		
+
+		//발주 정보
+		if(purchaseTsCode != null && !"".equals(purchaseTsCode)) {
+			 PurchaseTransaction purchaseTransactionInfo1 = purchaseTransactionService.getPurchaseTransactionInfoByCode(purchaseTsCode);
+			model.addAttribute("purchaseTransactionInfo1", purchaseTransactionInfo1);
+			System.out.println("purchaseTransactionInfo1-->" + purchaseTransactionInfo1);
+		}		
+					
+		
+		
+		 List<MainBusiness> mainBusinessList = mainBusinessService.getAllMainBusinessList();
+		  model.addAttribute("mainBusinessList", mainBusinessList);
+		  System.out.println("mainBusinessList" + mainBusinessList);
+		  
+		  List<Stock> itemList = itemService.getAllItemList(paramMap);
+		  
+		  model.addAttribute("itemList", itemList);
+		  System.out.println("itemList" + itemList);
+		  
+		  
+		  List<Contract> contractCodeForMaterialOrderCodeList = contractService.getContractCodeForMaterialOrderCodeList();
+		  model.addAttribute("contractCodeForMaterialOrderCodeList", contractCodeForMaterialOrderCodeList);
+		  System.out.println("contractCodeForMaterialOrderCodeList" + contractCodeForMaterialOrderCodeList);
+		  
+		  List<Contract> contractList = contractService.getAllContractList();
+		  model.addAttribute("contractList", contractList);
+		  System.out.println("contractList" + contractList);
+		  
+		  List<Client> clientList = clientService.getAllClientList();
+		  model.addAttribute("clientList", clientList);
+		  System.out.println("clientList: " + clientList);
+		  
+		  List<User> userList = userService.getAllUserList();
+		  model.addAttribute("userList", userList);
+		  System.out.println("userList" + userList);
+		  
+
+		model.addAttribute("title", "비용거래명세서: 수정");
+		model.addAttribute("purchaseTsCode", purchaseTsCode);
+		
+		return "purchaseTransaction/purchaseTransaction_modify";
 	}
 	
 	//비용상세
